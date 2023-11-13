432f81d924b4af4ab84156f8f68184bdcaeaa0e6
3bdo elnabawi <abdoelnabawi55@gmail.com>
2023-11-13 12:25:10 +0200

    nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { propertycardcomponent} from './property/property/property.component'
+import { PropertyListComponent} from './property/property-list/property-list/property-list.component'
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [		
+    AppComponent,
+    propertycardcomponent,
+    PropertyListComponent,
+      NavBarComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
---
+++ src/app/property/property/property.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+
+
+@Component({
+    selector :'app-property-card',
+    templateUrl : 'property.component.html',
+    styleUrls : ['property.component.html']
+
+})
+export class propertycardcomponent{
+
+}
