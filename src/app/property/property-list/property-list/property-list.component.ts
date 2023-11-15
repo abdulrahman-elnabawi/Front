24568ce69b711e617d584fe8b121904e9a5b9a23
3bdo elnabawi <abdoelnabawi55@gmail.com>
2023-11-15 20:26:34 +0200

    loop to make maney cards

--- src/app/property/property-list/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list/property-list.component.ts
@@ -6,5 +6,133 @@ import { Component } from '@angular/core';
   styleUrls: ['./property-list.component.css']
 })
 export class PropertyListComponent {
+  Properties : Array<any> =[  
+  {
+  "Id":1,
+  "Name":"abdo",
+  "Type":"Hoss",
+  "Price":12063
 
+  },
+  {
+  "Id":1,
+  "Name":"abdom",
+  "Type":"Ho",
+  "Price":12055
+
+  },
+  {
+  "Id":1,
+  "Name":"abdok",
+  "Type":"Housee",
+  "Price":12005
+
+  },
+  {
+  "Id":1,
+  "Name":"abdoc",
+  "Type":"Hou",
+  "Price":120005
+  },
+  {
+  "Id":1,
+  "Name":"abdol",
+  "Type":"Hous",
+  "Price":1200
+
+  },
+  {
+  "Id":1,
+  "Name":"abdol",
+  "Type":"Hous",
+  "Price":1200
+
+  },
+  {
+  "Id":1,
+  "Name":"abdol",
+  "Type":"Hous",
+  "Price":1200
+
+  }, {
+  "Id":1,
+  "Name":"abdol",
+  "Type":"Hous",
+  "Price":1200
+
+  }, {
+  "Id":1,
+  "Name":"abdol",
+  "Type":"Hous",
+  "Price":1200
+
+  },
+  {
+  "Id":1,
+  "Name":"abdol",
+  "Type":"Hous",
+  "Price":1200
+
+  }, {
+  "Id":1,
+  "Name":"abdol",
+  "Type":"Hous",
+  "Price":1200
+
+  }, {
+  "Id":1,
+  "Name":"abdol",
+  "Type":"Hous",
+  "Price":1200
+
+  }, {
+  "Id":1,
+  "Name":"abdol",
+  "Type":"Hous",
+  "Price":1200
+
+  }, {
+  "Id":1,
+  "Name":"abdol",
+  "Type":"Hous",
+  "Price":1200
+
+  }, {
+  "Id":1,
+  "Name":"abdol",
+  "Type":"Hous",
+  "Price":1200
+
+  }, {
+  "Id":1,
+  "Name":"abdol",
+  "Type":"Hous",
+  "Price":1200
+
+  }, {
+  "Id":1,
+  "Name":"abdol",
+  "Type":"Hous",
+  "Price":1200
+
+  }, {
+  "Id":1,
+  "Name":"abdol",
+  "Type":"Hous",
+  "Price":1200
+
+  }, {
+  "Id":1,
+  "Name":"abdol",
+  "Type":"Hous",
+  "Price":1200
+
+  }, {
+  "Id":1,
+  "Name":"abdol",
+  "Type":"Hous",
+  "Price":1200
+
+  },
+]
 }
